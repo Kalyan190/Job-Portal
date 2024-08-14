dd337f51492a1cddaebdc9061c065fa766a0380e
Kalyan Kumar <kalyaniit795@gmail.com>
2024-08-15 03:54:59 +0530

    api design

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,9 @@ import cookieParser from "cookie-parser";
 import cors from "cors";
 import dotenv from "dotenv"
 import connectDb from "./utils/database.js";
+import userRoute from '../backend/routes/user.routes.js'
+import companyRoute from '../backend/routes/company.route.js'
+import jobRoute from '../backend/routes/job.routes.js'
 
 dotenv.config({})
 
@@ -11,11 +14,11 @@ const app = express();
 
 //use middleware
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser())
 const corsOptions = {
-      origin:'http/localhost:5173',
-      credentials:true
+      origin: 'http/localhost:5173',
+      credentials: true
 }
 
 app.use(cors(corsOptions))
@@ -23,10 +26,12 @@ app.use(cors(corsOptions))
 
 const PORT = process.env.PORT || 3000;
 
+app.use("/api/v1/users/", userRoute)
+app.use("/api/v1/company/",companyRoute);
+app.use("/api/v1/job/",jobRoute);
 
-
-app.listen(PORT,()=>{
+app.listen(PORT, () => {
       connectDb();
       console.log(`Server running at port ${PORT}`);
-      }
-      )
+}
+)
