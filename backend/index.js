5ad481b5c0e7e70c5d7f9f29aed523653ee92759
Kalyan Kumar <kalyaniit795@gmail.com>
2024-08-22 19:52:43 +0530

    error resolve

--- backend/index.js
+++ backend/index.js
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser())
 const corsOptions = {
-   origin: 'https://job-portal-p7s6.vercel.app/',
+   origin: 'https://job-portal-smoky-two.vercel.app/',
       credentials: true
 }
 app.use(cors(corsOptions))
